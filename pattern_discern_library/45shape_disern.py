0f1218fb3579e137b26230923269c80ed7b6ea7d
KAWALAB40 <giver538@mail.com>
2021-03-30 21:17:45 +0900

    四角形・五角形のほうはパターン識別までやった

--- pattern_discern_library/45shape_disern.py
+++ pattern_discern_library/45shape_disern.py
@@ -13,12 +13,12 @@ plt.show()
 # cv2.imshow('output_shapes.png', img)
 # しきい値指定によるフィルタリング
 _, threshold = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY)
-# _, threshold = cv2.threshold(img, 230, 255, cv2.THRESH_BINARY)
+# _, threshold = cv2.threshold(img, 110, 255, cv2.THRESH_BINARY)
 
 # 輪郭を抽出
 # _, contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
 font = cv2.FONT_HERSHEY_DUPLEX
 
@@ -48,9 +48,11 @@ for cnt in contours:
     lencenterlist.append(len(cnt.T[0][0]))
 
 lencenterlist = np.argsort(lencenterlist)[::-1]
+imagelist = []
 for cnt in range(2):
     approx = approx_list[lencenterlist[cnt]]
-    cv2.drawContours(img, [approx], 0, (0, 0, 250), 2)
+    print("approx", approx)
+    # cv2.drawContours(img, [approx], 0, (0, 0, 250), 2)
     x = approx.ravel()[0]
     y = approx.ravel()[1]
     if len(approx) == 3:
@@ -59,7 +61,7 @@ for cnt in range(2):
 
     elif len(approx) == 4:
         rectangle += 1
-        cv2.putText(img, "rectangle{}".format(rectangle), (x, y), font, 0.8, (250, 250, 0))
+        # cv2.putText(img, "rectangle{}".format(rectangle), (x, y), font, 0.8, (250, 250, 0))
 
     elif len(approx) == 5:
         pentagon += 1
@@ -67,61 +69,72 @@ for cnt in range(2):
 
     elif 6 < len(approx) < 14:
         oval += 1
-        cv2.putText(img, "oval{}".format(oval), (x, y), font, 0.8, (0))
+        # cv2.putText(img, "oval{}".format(oval), (x, y), font, 0.8, (0))
 
     else:
         circle += 1
         # cv2.putText(img, "circle{}".format(circle), (x, y), font, 0.8, (250, 250, 0))
+#-----------------------変なところにある頂点をカットする。すでにopencvで隣り合う同士は並べられているので、それが小さすぎるものを省く------------------------------------
+Kado1 = approx_list[lencenterlist[0]]
+Kado2 = approx_list[lencenterlist[1]]
+newKado1, newKado2 = [], []
+length1 = 0
+length2 = 0
+length1list = []
+length2list = []
+for i in range(len(Kado1)):
+    if i == 0:
+        length1 += np.linalg.norm(Kado1[1] - Kado1[0]) + np.linalg.norm(Kado1[-1] - Kado1[0])
+        length1list.append([np.linalg.norm(Kado1[1] - Kado1[0]), np.linalg.norm(Kado1[-1] - Kado1[0])])
+    elif i == int(len(Kado1)) - 1:
+        length1 += np.linalg.norm(Kado1[-1] - Kado1[-2]) + np.linalg.norm(Kado1[-1] - Kado1[0])
+        length1list.append([np.linalg.norm(Kado1[-1] - Kado1[-2]), np.linalg.norm(Kado1[-1] - Kado1[0])])
+    else:
+        length1 += np.linalg.norm(Kado1[i] - Kado1[i-1]) + np.linalg.norm(Kado1[i] - Kado1[i+1])
+        length1list.append([np.linalg.norm(Kado1[i] - Kado1[i-1]), np.linalg.norm(Kado1[i] - Kado1[i+1])])
+normlen1 = length1/np.linalg.norm(int(len(Kado1)))
+for i in range(len(Kado1)):
+    if length1list[i][0] > normlen1/5:#しきい値は試行錯誤しているので、ロバストとは言い難い
+        newKado1.append(Kado1[i])
+
+for i in range(len(Kado2)):
+    if i == 0:
+        length2 += np.linalg.norm(Kado2[0] - Kado2[-1]) + np.linalg.norm(Kado2[1] - Kado2[0])
+        length2list.append([np.linalg.norm(Kado2[0] - Kado2[-1]), np.linalg.norm(Kado2[1] - Kado2[0])])
+    elif i == int(len(Kado2)) - 1:
+        length2 += np.linalg.norm(Kado2[-1] - Kado2[-2]) + np.linalg.norm(Kado2[-1] - Kado2[0])
+        length2list.append([np.linalg.norm(Kado2[-1] - Kado2[-2]), np.linalg.norm(Kado2[-1] - Kado2[0])])
+    else:
+        length2 += np.linalg.norm(Kado2[i] - Kado2[i-1]) + np.linalg.norm(Kado2[i] - Kado2[i+1])
+        length2list.append([np.linalg.norm(Kado2[i] - Kado2[i-1]), np.linalg.norm(Kado2[i] - Kado2[i+1])])
+normlen2 = length2/np.linalg.norm(int(len(Kado2)))
+print("normlen2", normlen2)
+for i in range(len(Kado2)):
+    print("normlen", length2list[i])
+    if length2list[i][0] > normlen2/3:
+        newKado2.append(Kado2[i])
+
+KadoCount1 = int(len(newKado1))#頂点の個数
+KadoCount2 = int(len(newKado2))#頂点の個数
+# KadoCount1 = int(len(approx_list[lencenterlist[0]]))
+# KadoCount2 = int(len(approx_list[lencenterlist[0]]))
+print("kadolist", [KadoCount1, KadoCount2])
+if KadoCount1 > KadoCount2:
+    cv2.drawContours(img, [approx_list[lencenterlist[0]]], 0, (0, 0, 250), 2)
+    cv2.drawContours(img, [approx_list[lencenterlist[1]]], 0, (250, 0, 0), 2)
+    # cv2.circle(img, (centers[lencenterlist[0]][0], centers[lencenterlist[0]][1]), 10, (0, 0, 200), thickness=-1)
+    cv2.putText(img, "A", (centers[lencenterlist[0]][0], centers[lencenterlist[0]][1]), font, 0.6, (0, 0, 200))
+    # cv2.circle(img, (centers[lencenterlist[1]][0], centers[lencenterlist[1]][1]), 10, (200, 0, 0), thickness=-1)
+    cv2.putText(img, "B", (centers[lencenterlist[1]][0], centers[lencenterlist[1]][1]), font, 0.6, (200, 0, 0))
+else:
+    cv2.drawContours(img, [approx_list[lencenterlist[0]]], 0, (250, 0, 0), 2)
+    cv2.drawContours(img, [approx_list[lencenterlist[1]]], 0, (0, 0, 250), 2)
+    # cv2.circle(img, (centers[lencenterlist[0]][0], centers[lencenterlist[0]][1]), 10, (200, 0, 0), thickness=-1)
+    cv2.putText(img, "B", (centers[lencenterlist[0]][0], centers[lencenterlist[0]][1]), font, 0.6, (200, 0, 0))
+    # cv2.circle(img, (centers[lencenterlist[1]][0], centers[lencenterlist[1]][1]), 10, (0, 0, 200), thickness=-1)
+    cv2.putText(img, "A", (centers[lencenterlist[1]][0], centers[lencenterlist[1]][1]), font, 0.6, (0, 0, 200))
 # 結果の画像作成
-cv2.imwrite('flycapture_exper_room/210303/pattern2/naname/PNG2021-03-03-204707.png', img)
-# lencenterlist = np.argsort(lencenterlist)[::-1]
-# centerlist = []
-# for i in range(len(lencenterlist)):
-#     centerlist.append(centers[lencenterlist[i]])
-# print(centerlist)
-# print(lencenterlist)
-# # マーカーの識別
-# # まずは組み合わせを作る
-# Conbination = []
-# for i in range(9):
-#     for j in range(i):
-#         for k in range(j):
-#             for l in range(k):
-#                 conc = []
-#                 conb = [i, j, k, l]
-#                 for m in range(9):
-#                     if m not in conb:
-#                         conc.append(m)
-#                 Conbination.append([conb, conc])
-#
-# print("Conbination", Conbination)
-# Flist = []
-# for i in range(len(Conbination)):
-#     # F = np.linalg.norm(centerlist[Conbination[i][0][0]] - centerlist[Conbination[i][0][1]]) ** 2 + np.linalg.norm(centerlist[Conbination[i][1][0]] - centerlist[Conbination[i][1][1]]) ** 2 + np.linalg.norm(centerlist[Conbination[i][1][0]] - centerlist[Conbination[i][1][2]]) ** 2 + np.linalg.norm(centerlist[Conbination[i][1][1]] - centerlist[Conbination[i][1][2]]) ** 2
-#     F = 0
-#     for j in range(4):
-#         for k in range(j):
-#             F += np.linalg.norm(centerlist[Conbination[i][0][j]] - centerlist[Conbination[i][0][k]]) ** 2
-#     for j in range(5):
-#         for k in range(j):
-#             F += np.linalg.norm(centerlist[Conbination[i][1][j]] - centerlist[Conbination[i][1][k]]) ** 2
-#     Flist.append(F)
-#     print(F)
-#
-# minindex = np.argmin(Flist)
-# minconb = Conbination[minindex]
-# print("minconb", minconb)
-#
-# print(centerlist)
-# cv2.circle(img, (centerlist[minconb[0][0]][0], centerlist[minconb[0][0]][1]), 10, (200, 0, 0), thickness=-1)
-# cv2.circle(img, (centerlist[minconb[0][1]][0], centerlist[minconb[0][1]][1]), 10, (200, 0, 0), thickness=-1)
-# cv2.circle(img, (centerlist[minconb[0][2]][0], centerlist[minconb[0][2]][1]), 10, (200, 0, 0), thickness=-1)
-# cv2.circle(img, (centerlist[minconb[0][3]][0], centerlist[minconb[0][3]][1]), 10, (200, 0, 0), thickness=-1)
-# cv2.circle(img, (centerlist[minconb[1][0]][0], centerlist[minconb[1][0]][1]), 10, (0, 0, 200), thickness=-1)
-# cv2.circle(img, (centerlist[minconb[1][1]][0], centerlist[minconb[1][1]][1]), 10, (0, 0, 200), thickness=-1)
-# cv2.circle(img, (centerlist[minconb[1][2]][0], centerlist[minconb[1][2]][1]), 10, (0, 0, 200), thickness=-1)
-# cv2.circle(img, (centerlist[minconb[1][3]][0], centerlist[minconb[1][3]][1]), 10, (0, 0, 200), thickness=-1)
-# cv2.circle(img, (centerlist[minconb[1][4]][0], centerlist[minconb[1][4]][1]), 10, (0, 0, 200), thickness=-1)
+cv2.imwrite('flycapture_exper_room/210303/pattern2/seitsui/PNG2021-03-03-204831.png', img)
 
 
 # 図形の数の結果
